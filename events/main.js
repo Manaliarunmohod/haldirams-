1e2d22a1faff29e86c4aebe9fa0c51433f8c54fa
Manali mohod <masaimanali@gmail.com>
2022-02-02 17:58:29 +0530

    change

---
+++ events/main.js
@@ -0,0 +1,29 @@
+const EventEmitter=require("events")
+
+const eventEmitter=new EventEmitter()
+
+function userRegister(){
+    event.Emitter.emit("User Register");
+
+event.Emitter.on("User Registration",()=>{
+
+console.log("send verification")
+
+});
+
+event.Emitter.on("User Registration",()=>{
+
+    console.log("send verification")
+    
+    });
+    event.Emitter.on("User Registration",()=>{
+
+        console.log("send verification")
+        
+        });
+        event.emitter.email("Users Registers")
+}
+
+
+
+userRegister();
