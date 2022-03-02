8ef74bcddff6ccf74969b6e46a4f0622d263cee1
Manali mohod <masaimanali@gmail.com>
2022-03-02 09:07:29 +0530

    commit

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const makesandwitch=require("./add")
+// const makesandwitch=require("./add")
 
-console.log(makesandwitch);
+// console.log(makesandwitch);
 
-makesandwitch.makesandwitch();
+// makesandwitch.makesandwitch();
 
-function Something(a){
-    a=2;
-    return a;
-}
-let a=1;
-console.log(Something(a))
-console.log(a);
+// function Something(a){
+//     a=2;
+//     return a;
+// }
+// let a=1;
+// console.log(Something(a))
+// console.log(a);
 
-function Somehing(b){
-    b= {name:"mina"};
-    return b;
-}
-let b={name:"mani"};
+// function Somehing(b){
+//     b= {name:"mina"};
+//     return b;
+// }
+// let b={name:"mani"};
 
-console.log(Somehing(b))
-console.log(b);
+// console.log(Somehing(b))
+// console.log(b);
 
+// console.log(makesandwitch.nam);
 
-
-let c={name:"pihu"};
-let d=c;
-d.name="janu";
-console.log(c);
-console.log(d);
+// let c={name:"pihu"};
+// let d=c;
+// d.name="janu";
+// console.log(c);
+// console.log(d);
